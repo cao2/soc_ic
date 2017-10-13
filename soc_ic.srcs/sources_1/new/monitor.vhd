04a4cd226741f15204c69b99a3074022696403f0
cao2 <cao2@mail.usf.edu>
2017-10-13 18:18:20 -0400

    .

--- soc_ic.srcs/sources_1/new/monitor.vhd
+++ soc_ic.srcs/sources_1/new/monitor.vhd
@@ -8,77 +8,77 @@ entity monitor is
 		constant interface_type : positive := 1
 	);
 	Port(
-		clk           : in  STD_LOGIC;
-		rst           : in  STD_LOGIC;
+		clk            : in  STD_LOGIC;
+		rst            : in  STD_LOGIC;
 		----AXI interface
-		master_id : in std_logic_vector(IP_CT downto 0);
-		slave_id: in std_logic_vector(IP_CT downto 0);
-		id_i          : in  IP_T;
+		master_id      : in  std_logic_vector(IP_CT downto 0);
+		slave_id       : in  std_logic_vector(IP_CT downto 0);
+		id_i           : in  IP_T;
 		---write address channel
-		waddr_i       : in  ADR_T;
-		wlen_i        : in  std_logic_vector(9 downto 0);
-		wsize_i       : in  std_logic_vector(9 downto 0);
-		wvalid_i      : in  std_logic;
-		wready_i      : in  std_logic;
+		waddr_i        : in  ADR_T;
+		wlen_i         : in  std_logic_vector(9 downto 0);
+		wsize_i        : in  std_logic_vector(9 downto 0);
+		wvalid_i       : in  std_logic;
+		wready_i       : in  std_logic;
 		---write data channel
-		wdata_i       : in  std_logic_vector(31 downto 0);
-		wtrb_i        : in  std_logic_vector(3 downto 0); --TODO not implemented
-		wlast_i       : in  std_logic;
-		wdvalid_i     : in  std_logic;
-		wdataready_i  : in  std_logic;
+		wdata_i        : in  std_logic_vector(31 downto 0);
+		wtrb_i         : in  std_logic_vector(3 downto 0); --TODO not implemented
+		wlast_i        : in  std_logic;
+		wdvalid_i      : in  std_logic;
+		wdataready_i   : in  std_logic;
 		---write response channel
-		wrready_i     : in  std_logic;
-		wrvalid_i     : in  std_logic;
-		wrsp_i        : in  std_logic_vector(1 downto 0);
+		wrready_i      : in  std_logic;
+		wrvalid_i      : in  std_logic;
+		wrsp_i         : in  std_logic_vector(1 downto 0);
 		---read address channel
-		raddr_i       : in  std_logic_vector(31 downto 0);
-		rlen_i        : in  std_logic_vector(9 downto 0);
-		rsize_i       : in  std_logic_vector(9 downto 0);
-		rvalid_i      : in  std_logic;
-		rready_i      : in  std_logic;
+		raddr_i        : in  std_logic_vector(31 downto 0);
+		rlen_i         : in  std_logic_vector(9 downto 0);
+		rsize_i        : in  std_logic_vector(9 downto 0);
+		rvalid_i       : in  std_logic;
+		rready_i       : in  std_logic;
 		---read data channel
-		rdata_i       : in  std_logic_vector(31 downto 0);
-		rstrb_i       : in  std_logic_vector(3 downto 0);
-		rlast_i       : in  std_logic;
-		rdvalid_i     : in  std_logic;
-		rdready_i     : in  std_logic;
-		rres_i        : in  std_logic_vector(1 downto 0);
+		rdata_i        : in  std_logic_vector(31 downto 0);
+		rstrb_i        : in  std_logic_vector(3 downto 0);
+		rlast_i        : in  std_logic;
+		rdvalid_i      : in  std_logic;
+		rdready_i      : in  std_logic;
+		rres_i         : in  std_logic_vector(1 downto 0);
 		----output 
-		id_o          : out IP_T;
+		id_o           : out IP_T;
 		---write address channel
-		waddr_o       : out ADR_T;
-		wlen_o        : out std_logic_vector(9 downto 0);
-		wsize_o       : out std_logic_vector(9 downto 0);
-		wvalid_o      : out std_logic;
-		wready_o      : out std_logic;
+		waddr_o        : out ADR_T;
+		wlen_o         : out std_logic_vector(9 downto 0);
+		wsize_o        : out std_logic_vector(9 downto 0);
+		wvalid_o       : out std_logic;
+		wready_o       : out std_logic;
 		---write data channel
-		wdata_o       : out std_logic_vector(31 downto 0);
-		wtrb_o        : out std_logic_vector(3 downto 0); --TODO not implemented
-		wlast_o       : out std_logic;
-		wdvalid_o     : out std_logic;
-		wdataready_o  : out std_logic;
+		wdata_o        : out std_logic_vector(31 downto 0);
+		wtrb_o         : out std_logic_vector(3 downto 0); --TODO not implemented
+		wlast_o        : out std_logic;
+		wdvalid_o      : out std_logic;
+		wdataready_o   : out std_logic;
 		---write response channel
-		wrready_o     : out std_logic;
-		wrvalid_o     : out std_logic;
-		wrsp_o        : out std_logic_vector(1 downto 0);
+		wrready_o      : out std_logic;
+		wrvalid_o      : out std_logic;
+		wrsp_o         : out std_logic_vector(1 downto 0);
 		---read address channel
-		raddr_o       : out std_logic_vector(31 downto 0);
-		rlen_o        : out std_logic_vector(9 downto 0);
-		rsize_o       : out std_logic_vector(9 downto 0);
-		rvalid_o      : out std_logic;
-		rready_o      : out std_logic;
+		raddr_o        : out std_logic_vector(31 downto 0);
+		rlen_o         : out std_logic_vector(9 downto 0);
+		rsize_o        : out std_logic_vector(9 downto 0);
+		rvalid_o       : out std_logic;
+		rready_o       : out std_logic;
 		---read data channel
-		rdata_o       : out std_logic_vector(31 downto 0);
-		rstrb_o       : out std_logic_vector(3 downto 0);
-		rlast_o       : out std_logic;
-		rdvalid_o     : out std_logic;
-		rdready_o     : out std_logic;
-		rres_o        : out std_logic_vector(1 downto 0);
+		rdata_o        : out std_logic_vector(31 downto 0);
+		rstrb_o        : out std_logic_vector(3 downto 0);
+		rlast_o        : out std_logic;
+		rdvalid_o      : out std_logic;
+		rdready_o      : out std_logic;
+		rres_o         : out std_logic_vector(1 downto 0);
 		------Customized interface
 
-		inter2_i      : in  MSG_T;
-		inter2_o      : out MSG_T;
-		transaction_o : out TST_T;
+		inter2_i       : in  MSG_T;
+		inter2_o       : out MSG_T;
+		transaction_o  : out TST_T;
 		transaction_o2 : out TST_T
 	);
 end monitor;
@@ -88,108 +88,107 @@ architecture rtl of monitor is
 	--signal td2: std_logic_vector(31 downto 0);
 	type state is (one, two, three, four, five);
 begin
-	
+
 	axi_wt_extractor_write : process(clk)
 		variable tmp_transaction : TST_T;
-		variable st: state := one;
+		variable st              : state := one;
 	begin
 		if rising_edge(clk) then
 			if st = one then
 				if interface_type = 0 then
-					if wready_i ='1' then
-						st :=two;
+					if wready_i = '1' then
+						st := two;
 					end if;
 				end if;
 			elsif st = two then
-				if wvalid_i ='1' then
-				    tmp_transaction.sender := master_id;
-				    tmp_transaction.receiver := slave_id;
-					tmp_transaction.cmd := WRITE_CMD;
+				if wvalid_i = '1' then
+					tmp_transaction.sender   := master_id;
+					tmp_transaction.receiver := slave_id;
+					tmp_transaction.cmd      := WRITE_CMD;
 					---ATTENTION: addr need to be taken care of
 					---so is tag and id
-					tmp_transaction.adr := "00";
+					tmp_transaction.adr      := "00";
 					---Note: there are also size, and length, ignored here
-					st:= three;
+					st                       := three;
 				end if;
 			elsif st = three then
-				if wdataready_i ='1' then
+				if wdataready_i = '1' then
 					---Note: the data is available here
 					---, do we need to check that?
-					if wdvalid_i ='1' and wlast_i='1' then
-					   st := four;
+					if wdvalid_i = '1' and wlast_i = '1' then
+						st            := four;
 						---read response here is done
 						transaction_o <= tmp_transaction;
 					end if;
 				end if;
 			elsif st = four then
-			transaction_o.val<='0';
-			if wrvalid_i='1' then
-			 if wrsp_i ="00" then
-			     tmp_transaction.sender:=slave_id;
-			     tmp_transaction.receiver:=master_id;
-			     tmp_transaction.cmd := WRITE_CMD;
-                 transaction_o <= tmp_transaction;
-                 st := one;
-             end if;
-			end if;
+				transaction_o.val <= '0';
+				if wrvalid_i = '1' then
+					if wrsp_i = "00" then
+						tmp_transaction.sender   := slave_id;
+						tmp_transaction.receiver := master_id;
+						tmp_transaction.cmd      := WRITE_CMD;
+						transaction_o            <= tmp_transaction;
+						st                       := one;
+					end if;
+				end if;
 			end if;
 		end if;
 	end process;
-	
-	
+
 	axi_rd_extractor : process(clk)
 		variable tmp_transaction : TST_T;
-		variable st: state := one;
+		variable st              : state := one;
 	begin
 		if rising_edge(clk) then
 			if st = one then
-			    transaction_o2.val <='0';
+				transaction_o2.val <= '0';
 				if interface_type = 0 then
 					----axi protocol
 					----read protocol
-					if rready_i ='1' then
-						st :=two;
+					if rready_i = '1' then
+						st := two;
 					end if;
 				elsif interface_type = 1 then
-					if inter2_i.val='1' then
-					tmp_transaction.val:='1';
-					tmp_transaction.cmd:=inter2_i.cmd;
-					----these three fields need to be carfully considered
-					tmp_transaction.adr:="00";
-					tmp_transaction.tag:="00";
-					tmp_transaction.id:= "00";
-					transaction_o2 <= tmp_transaction;
+					if inter2_i.val = '1' then
+						tmp_transaction.val := '1';
+						tmp_transaction.cmd := inter2_i.cmd;
+						----these three fields need to be carfully considered
+						tmp_transaction.adr := "00";
+						tmp_transaction.tag := "00";
+						tmp_transaction.id  := "00";
+						transaction_o2      <= tmp_transaction;
 					end if;
 				end if;
 			elsif st = two then
-				if rvalid_i ='1' then
-					tmp_transaction.cmd := READ_CMD;---set the c
+				if rvalid_i = '1' then
+					tmp_transaction.cmd      := READ_CMD; ---set the c
 					---ommand => don't remmeber the specific code, 
 					---change whenever have time
 					---ATTENTION: addr need to be taken care of
 					---so is tag and id
-					tmp_transaction.adr := "00";
-					tmp_transaction.sender := master_id;
+					tmp_transaction.adr      := "00";
+					tmp_transaction.sender   := master_id;
 					tmp_transaction.receiver := slave_id;
-					transaction_o2 <= tmp_transaction;
+					transaction_o2           <= tmp_transaction;
 					---Note: there are also size, and length, ignored here
-					st:= three;
+					st                       := three;
 				end if;
 			elsif st = three then
-			    transaction_o2.val <='0';
-				if rdready_i ='1' then
-				   st := four;
+				transaction_o2.val <= '0';
+				if rdready_i = '1' then
+					st := four;
 				end if;
-			elsif st=four then
-			  ---Note: the data is available here
-                                ---, do we need to check that?
-                                if rdvalid_i='1' and rlast_i='1' and  rres_i="00" then
-                                    ---read response here is done
-                                    tmp_transaction.sender := slave_id;
-                                    tmp_transaction.receiver := master_id;
-                                    transaction_o2 <= tmp_transaction;
-                                    st := one;
-                                end if;
+			elsif st = four then
+				---Note: the data is available here
+				---, do we need to check that?
+				if rdvalid_i = '1' and rlast_i = '1' and rres_i = "00" then
+					---read response here is done
+					tmp_transaction.sender   := slave_id;
+					tmp_transaction.receiver := master_id;
+					transaction_o2           <= tmp_transaction;
+					st                       := one;
+				end if;
 			end if;
 		end if;
 	end process;
