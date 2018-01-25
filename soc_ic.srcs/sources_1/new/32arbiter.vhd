56d3f06e53befff9210d0e4ed5f0e2d0b1fc30a8
cao2 <cao2@mail.usf.edu>
2018-01-25 17:46:05 -0500

    121

--- soc_ic.srcs/sources_1/new/32arbiter.vhd
+++ soc_ic.srcs/sources_1/new/32arbiter.vhd
@@ -47,7 +47,7 @@ entity arbiter32 is
 end arbiter32;
 
 architecture rtl of arbiter32 is
-	constant dpth                                                                                                                                                                                                               : positive                      := 3;
+	constant dpth                                                                                                                                                                                                               : positive                      := 2;
 	--signal td1: std_logic_vector(31 downto 0);
 	--signal td2: std_logic_vector(31 downto 0);
 	signal in0, in1, in2, in3, in4, in5, in6, in7, in8, in9, in10, in11, in12, in13, in14, in15, in16, in17, in18, in19, in20, in21, in22, in23, in24, in25, in26, in27, in28, in29, in30, in31                                 : std_logic_vector(36 downto 0);
